fb74ec0998d1fe11dbe6fc1f2413171fbe3eac1a
I-AM-SENTIENT <am.i.sentient.i.am@gmail.com>
2025-12-29 19:01:06 +0100

    Add files via upload

---
+++ Li Isotopes 2.1.py
@@ -0,0 +1,171 @@
+# A python-based data reduction scheme for iolite 4 starts with some metadata
+#/ Type: DRS
+#/ Name: Li isotopes 2.0
+#/ Authors: Mati i Robert
+#/ Description: A thing
+#/ References:
+#/ Version:
+#/ Contact:
+
+"""
+Before functions are called a few additional objects are
+added to the module:
+
+data	an interface to iolite's C++ data. E.g. you can get
+        existing time series data or selection groups with it
+        as well as make new ones.
+
+IoLog	an interface to iolite's logging facility. You can add
+        messages with, e.g., IoLog.debug('My message')
+
+drs	an interface to the PythonDRS C++ class in iolite from
+        which some built-in features can be accessed, e.g.,
+        baselineSubtract(group, channels, mask)
+
+Qt imports can be done through 'iolite', e.g.
+from iolite.QtGui import QLabel
+"""
+
+from iolite import QtGui
+from time import sleep
+import numpy as np
+
+
+def runDRS():
+    """
+    This method will be called by iolite when the user clicks
+    Crunch Data in the DRS window or as part of a processing
+    template. It should transform 'input' data into 'output'
+    data using the provided settings.
+
+    DRS progress can be updated via the 'message' and 'progress'
+    signals. These will be displayed in the iolite interface.
+
+    When finished, the 'finished' signal should be emitted.
+
+    As an example, we will do baseline subtraction of all
+    input channels using a DRS helper function.
+    """
+
+    drs.message("Starting baseline subtract DRS...")
+    drs.progress(0)
+
+    # Get settings
+    settings = drs.settings()
+    print(settings)
+
+    indexChannel = data.timeSeries(settings["IndexChannel"])
+    maskChannel = data.timeSeries(settings["MaskChannel"])
+    cutoff = settings["MaskCutoff"]
+    trim = settings["MaskTrim"]
+    ref_mat = settings["ReferenceMaterial"]
+
+    # Create debug messages for the settings being used
+    IoLog.debug("indexChannelName = %s" % indexChannel.name)
+    IoLog.debug("maskChannelName = %s" % maskChannel.name)
+    IoLog.debug("maskCutoff = %f" % cutoff)
+    IoLog.debug("maskTrim = %f" % trim)
+
+    # Setup index time
+    drs.message("Setting up index time...")
+    drs.progress(5)
+    drs.setIndexChannel(indexChannel)
+
+    # Setup the mask
+    drs.message("Making mask...")
+    drs.progress(10)
+    mask = drs.createMaskFromCutoff(maskChannel, cutoff, trim)
+
+    # Interp onto index time and baseline subtract
+    drs.message("Interpolating onto index time and baseline subtracting...")
+    drs.progress(25)
+
+    allInputChannels = data.timeSeriesList(data.Input)
+    #Baseline subtraction
+    for counter, channel in enumerate(allInputChannels):
+        drs.message("Baseline subtracting %s" % channel.name)
+        drs.progress(25 + 75*counter/len(allInputChannels))
+        sleep(0.5) # Sleeping only so that the progress can be observed
+
+        drs.baselineSubtract(data.selectionGroup("Baseline"), [allInputChannels[counter]], mask, 25, 100)
+    
+    #Li7 to Li6 ratio
+    Li7_CPS = data.timeSeries('Li7_CPS').data()
+    Li6_CPS = data.timeSeries('Li6_CPS').data()
+    Li7_6Li = Li7_CPS / Li6_CPS
+    Li7_CPS_time = data.timeSeries("Li7_CPS").time()
+    #Output the ratio 
+    data.createTimeSeries("Li7/6Li",data.Intermediate,Li7_CPS_time,Li7_6Li)
+    
+    #Update
+    data.updateResults()
+    
+    #Calculate delta
+    SplineLi7_Li6 = data.spline(ref_mat, "Li7/6Li").data()
+    data.createTimeSeries("DupaSpline",data.Intermediate, Li7_CPS_time, SplineLi7_Li6)
+    
+    #Calculate delta 
+    Delta_Li7_6 = ((data.timeSeries('Li7/6Li').data() / SplineLi7_Li6)-1) *1000
+    data.createTimeSeries("Delta_Li7_6", data.Output, Li7_CPS_time, Delta_Li7_6)
+    
+        
+    drs.message("Finished!")
+    drs.progress(100)
+    drs.finished()
+
+
+def settingsWidget():
+    """
+    This function puts together a user interface to configure the DRS.
+
+    It is important to have the last line of this function call:
+    drs.setSettingsWidget(widget)
+    """
+
+    widget = QtGui.QWidget()
+    formLayout = QtGui.QFormLayout()
+    widget.setLayout(formLayout)
+
+    timeSeriesNames = data.timeSeriesNames(data.Input)
+    defaultChannelName = ""
+    if timeSeriesNames:
+            defaultChannelName = timeSeriesNames[0]
+
+    drs.setSetting("IndexChannel", defaultChannelName)
+    drs.setSetting("MaskChannel", defaultChannelName)
+    drs.setSetting("ReferenceMaterial",None)
+    drs.setSetting("MaskCutoff", 0.0)
+    drs.setSetting("MaskTrim", 0.0)
+
+    settings = drs.settings()
+
+    indexComboBox = QtGui.QComboBox(widget)
+    indexComboBox.addItems(data.timeSeriesNames(data.Input))
+    indexComboBox.setCurrentText(settings["IndexChannel"])
+    indexComboBox.currentTextChanged.connect(lambda t: drs.setSetting("IndexChannel", t))
+    
+    # ReferenceMaterial Selector
+    rmComboBox = QtGui.QComboBox(widget)
+    rmComboBox.addItems(data.referenceMaterialNames())
+    rmComboBox.currentTextChanged.connect(lambda s: drs.setSetting("ReferenceMaterial",str(s)))
+    
+    maskComboBox = QtGui.QComboBox(widget)
+    maskComboBox.addItems(data.timeSeriesNames(data.Input))
+    maskComboBox.setCurrentText(settings["MaskChannel"])
+    maskComboBox.currentTextChanged.connect(lambda t: drs.setSetting("MaskChannel", t))
+
+    maskLineEdit = QtGui.QLineEdit(widget)
+    maskLineEdit.setText(settings["MaskCutoff"])
+    maskLineEdit.textChanged.connect(lambda t: drs.setSetting("MaskCutoff", float(t)))
+
+    maskTrimLineEdit = QtGui.QLineEdit(widget)
+    maskTrimLineEdit.setText(settings["MaskTrim"])
+    maskTrimLineEdit.textChanged.connect(lambda t: drs.setSetting("MaskTrim", float(t)))
+
+    formLayout.addRow("Index channel", indexComboBox)
+    formLayout.addRow("Reference material", rmComboBox)
+    formLayout.addRow("Mask channel", maskComboBox)
+    formLayout.addRow("Mask cutoff", maskLineEdit)
+    formLayout.addRow("Mask trim", maskTrimLineEdit)
+
+    drs.setSettingsWidget(widget)
